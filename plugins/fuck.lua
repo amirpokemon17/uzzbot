dda68b0c7d327918f23db405af5ff70d7e786b1f
amirilk77 <amirilk77@gmail.com>
2016-01-03 01:29:40 +0330

    Create fuck.lua

---
+++ plugins/fuck.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return " kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf kiram to badkhah AmirBlackWolf "
+end
+
+return {
+  description = "سیس", 
+  usage = "/credits",
+  patterns = {
+    "^amir",
+    "^siktir",
+    "^kosnanat",
+  },
+  run = run
+}
+
+end
